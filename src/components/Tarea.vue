c5a09a74683ff778d63b04fb86cca2126846ff44
Marco Escorza <ca246033@uaeh.edu.mx>
2021-08-05 12:02:51 -0500

    Conectando backend con api

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="display-4 text-center">Listado de tareas </h1>
+        <h1 class="display-4 text-center">Listado de tareas UAEH v0.1 </h1>
         <hr>
         <div class="row">
             <div class="col-lg-8 offset-lg-2">
@@ -17,18 +17,24 @@
                         </div>
                         <br>
                         <!--{{listTareas}}-->
+
+                        <div class="text-center">
+                            <div v-if="loading" class="spinner-border text-success" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
                         <h5 v-if="listTareas.length == 0" class="text-center text-bold">No hay tareas para realizar</h5>
                         
                         <ul class="list-group">
                             <li v-for="(tarea,index) of listTareas" :key="index"
                             class="list-group-item d-flex justify-content-between">
                                 <span class="cursor" v-bind:class="{'text-success': tarea.estado}"
-                                v-on:click="editarTarea(tarea,index)">
+                                v-on:click="editarTarea(tarea,tarea.id)">
                                     <i v-bind:class="[tarea.estado ? 'fas fa-check-circle' : 'far fa-circle']"></i>
                                     
                                 </span>
                                 {{tarea.nombre}}
-                                <span class="text-danger cursor" v-on:click="eliminarTarea(index)">
+                                <span class="text-danger cursor" v-on:click="eliminarTarea(tarea.id, index)">
                                     <i class="fas fa-trash-alt"></i>
                                 </span>
                             </li>
@@ -42,12 +48,17 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
+    const URL = "https://backend-tareasservices.azurewebsites.net/api/Tarea/";
     export default {
        name: 'Tarea' ,
        data(){
            return{
                tarea: '',
-               listTareas :[]
+               listTareas :[],
+               loading: false
            }
        },
        methods:{
@@ -58,15 +69,45 @@
                   estado: false
               }
 
-              this.listTareas.push(tarea)
+            //   this.listTareas.push(tarea)
+            this.loading = true;
+            axios.post(URL,tarea).then(response =>{
+                console.log(response.data.message)
+                this.loading=false;
+                this.obtenerTareas();
+            }).catch(error =>{
+                console.error(error);
+                this.loading=false;
+            })
               this.tarea = '' 
            },
-           eliminarTarea(index){
-               this.listTareas.splice(index,1)
+           eliminarTarea(id, index){
+               //this.listTareas.splice(index,1)
+               axios.delete(URL+id).then(response =>{
+                   console.log(response.data.message);
+                   this.listTareas.splice(index,1)
+               }).catch(error => console.log(error))
+
            },
-           editarTarea(tarea, index){
-               this.listTareas[index].estado = !tarea.estado
+           editarTarea(tarea, id){
+            //    this.listTareas[index].estado = !tarea.estado
+            this.loading = true;
+                axios.put(URL+id,tarea).then(()=>{
+                    this.loading = false
+                    this.obtenerTareas()
+                }).catch(()=>this.loading = false);
+           },
+           
+           obtenerTareas(){
+               axios.get(URL).then(response =>{
+                   //console.log(response.data);
+                   this.listTareas = response.data;
+               })
            }
+       }
+       ,created: function(){
+           this.obtenerTareas();
+
        }
     }
 </script>
